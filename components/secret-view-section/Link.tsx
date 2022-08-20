3e57076f3d455e93309ce7ed675804f18bad850e
kingvyas <kumarvyasss@gmail.com>
2022-08-20 12:48:01 +0530

    added firebase

--- components/secret-view-section/Link.tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Link(): JSX.Element {
-  const router = useRouter();
-  const link = router.query.link;
-
-  return <div>{link}</div>;
-}
