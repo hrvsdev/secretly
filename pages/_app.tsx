5ca270a712bf378b4c402f0180f31b837905b378
kingvyas <kumarvyasss@gmail.com>
2022-08-21 09:47:52 +0530

    trying to fix github commit bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>One Time Secret 🤫 | Encrypted and Secure</title>
+        <meta
+          name="description"
+          content="Share a one-time secret. Secret Share lets you securely share sensitive data such as passwords, API keys, and OTPs using end-to-end encryption that is destroyed instantly after viewing once."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
