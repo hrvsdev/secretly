c3b51b32b06538fc042d80bfd748bf9fce5886a4
kingvyas <kumarvyasss@gmail.com>
2022-08-21 09:35:53 +0530

    removed head from index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,9 @@ const Home = dynamic(() => import("../components/home"), { suspense: true });
 
 const Root: NextPage = () => {
   return (
-    <div>
-      <Head>
-        <title>One Time Secret 🤫 | Encrypted and Secure</title>
-        <meta
-          name="description"
-          content="Share a one-time secret. Secret Share lets you securely share sensitive data such as passwords, API keys, and OTPs using end-to-end encryption that is destroyed instantly after viewing once."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Suspense fallback={<h1>Loading ...</h1>}>
-        <Home />
-      </Suspense>
-    </div>
+    <Suspense fallback={<h1>Loading ...</h1>}>
+      <Home />
+    </Suspense>
   );
 };
 
