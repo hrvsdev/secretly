c9ec433455faa55a0609ccd3941712465a79d73f
kingvyas <kumarvyasss@gmail.com>
2022-08-24 14:58:34 +0530

    adding secret enc and dec

---
+++ utils/utils.ts
@@ -0,0 +1,24 @@
+import { AES, enc } from "crypto-js";
+import { customAlphabet } from "nanoid";
+
+const genKey = () => {
+  const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const nanoid = customAlphabet(alphabet, 20);
+  return nanoid();
+};
+
+const encrypt = (string: string, key: string) => {
+  return AES.encrypt(string, key).toString();
+};
+
+const decrypt = (ciphertext: string, key: string) => {
+  const bytes = AES.decrypt(ciphertext, key);
+  return bytes.toString(enc.Utf8);
+};
+
+const genLink = (id: string, key: string) => {
+  return id + "#" + key;
+};
+
+
+export {genKey, encrypt, decrypt, genLink}
