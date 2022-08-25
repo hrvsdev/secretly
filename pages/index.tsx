0a810cf847f6bc2a57d26afca41c65d927bd431c
kingvyas <kumarvyasss@gmail.com>
2022-08-25 11:38:08 +0530

    removed dynamic import for Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
 import type { NextPage } from "next";
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
 
-const Home = dynamic(() => import("../components/home"), { suspense: true });
+import Home from "../components/home";
 
 const Root: NextPage = () => {
-  return (
-    <Suspense fallback={<h1>Loading ...</h1>}>
-      <Home />
-    </Suspense>
-  );
+  return <Home />;
 };
 
 export default Root;
