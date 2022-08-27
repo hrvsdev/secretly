bc84339c5d39015dbc56728da0ea685857bdcf55
kingvyas <kumarvyasss@gmail.com>
2022-08-27 10:37:33 +0530

    styled header of tab

--- components/secret-input-section/tabs/Tabs.tsx
+++ components/secret-input-section/tabs/Tabs.tsx
@@ -1,7 +1,16 @@
 import styled from "@emotion/styled";
 
 export default function Tabs() {
-  return <TabsWrapper><Tab>Hello</Tab></TabsWrapper>;
+  return (
+    <TabsWrapper>
+      <Header>
+        <Button active={true}>Password</Button>
+        <Button>Message</Button>
+        <Button>Read Reciept</Button>
+        <Button>Delivery</Button>
+      </Header>
+    </TabsWrapper>
+  );
 }
 
 const TabsWrapper = styled.div`
@@ -9,4 +18,23 @@ const TabsWrapper = styled.div`
   margin-bottom: 20px;
 `;
 
-const Tab = styled.div``
+const Header = styled.div`
+  height: 50px;
+  border-bottom: 2px solid rgba(255, 255, 255, 0.2);
+  display: flex;
+  flex-direction: row;
+`;
+
+const Button = styled.div<{ active?: true }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  margin-bottom: -2px;
+  padding: 0 16px;
+  cursor: pointer;
+  color: ${({ active }) => active && "#0072f5"};
+  border-bottom: ${({ active }) => active && "2px solid currentcolor"};
+`;
+
+const Content = styled.div``;
