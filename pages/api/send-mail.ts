e4e98bab844b4b26b383df171ce2610b63da7bb1
kingvyas <kumarvyasss@gmail.com>
2022-09-05 23:18:08 +0530

    changing email

--- pages/api/send-mail.ts
+++ pages/api/send-mail.ts
@@ -2,17 +2,17 @@ import nodemailer from "nodemailer";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const transporter = nodemailer.createTransport("SMTP", {
-        service: "hotmail",
-        auth: {
-            user: "scrtly@hotmail.com",
-            pass: "@Hrvs.dev822"
-        }
-    });
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "itzharshvyas@gmail.com",
+      pass: "harsh.123",
+    },
+  });
 
   try {
     const info = await transporter.sendMail({
-      from: "Secretly <scrtly@hotmail.com>",
+      from: "Secretly <i@hrvs.me>",
       to: "itsharshvyas@gmail.com",
       subject: "Hello ✔",
       text: "Hello world?",
