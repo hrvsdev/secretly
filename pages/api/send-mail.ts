a52d777418be666fc419b9a2ed6e771c1025a222
kingvyas <kumarvyasss@gmail.com>
2022-09-05 22:48:17 +0530

    added boilerplate code

--- pages/api/send-mail.ts
+++ pages/api/send-mail.ts
@@ -1,5 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import nodemailer from "nodemailer";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ text: "Hello" });
+  const testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false,
+    auth: {
+      user: testAccount.user, 
+      pass: testAccount.pass,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>',
+    to: "bar@example.com, baz@example.com",
+    subject: "Hello ✔",
+    text: "Hello world?",
+    html: "<b>Hello world?</b>",
+  });
+
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 }
