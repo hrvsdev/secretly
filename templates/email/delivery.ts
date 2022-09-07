01dc28ab439ea342d2cfaf1f83b0fee85f7ca242
kingvyas <kumarvyasss@gmail.com>
2022-09-07 22:58:24 +0530

    fixing ugly layout

--- templates/email/delivery.ts
+++ templates/email/delivery.ts
@@ -6,7 +6,7 @@ const deliveryTemp = (link: string, message?: string) => {
       Hello, <br>
       Someone has sent you a secret.
     </p>
-    <a href="${link}" style="text-decoration: none;"><button style="all: unset;height: 48px;color: #ffffff;background-color: #0072f5;border-radius: 10px;padding: 0 40px;margin: 20px 0;will-change: transform;transition: all 0.25s;">View Secret</button></a>
+    <a href="${link}" style="text-decoration: none;"><button style="border: 0;outline: 0;height: 48px;color: #ffffff;background-color: #0072f5;border-radius: 10px;padding: 0 40px;margin: 20px 0;will-change: transform;transition: all 0.25s;">View Secret</button></a>
     <p style="margin-bottom: 12px;font-size: 17px;line-height: 25px;margin: 0;">Be aware, it will be deleted instantly after opening.</p>
     <p style="font-size: 17px;line-height: 25px;margin: 0;">
       Thanks! <br>
