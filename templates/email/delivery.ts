eeb3a632ba10def3f51a15224bb977390a2e5b2c
kingvyas <kumarvyasss@gmail.com>
2022-09-07 22:30:21 +0530

    adding dynamic data on delivery

--- templates/email/delivery.ts
+++ templates/email/delivery.ts
@@ -1,22 +1,23 @@
 const delivery = (link: string, message?: string) => {
-  return `
-    <h1 style="font-family: -apple-system, system-ui, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, sans-serif;text-align: center;color: #0072f5;">
+  return `<h1 style="text-align: center;color: #0072f5;">
 Secretly
 </h1>
-<main style="font-family: -apple-system, system-ui, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, sans-serif;background: white;padding: 30px 20px;max-width: 600px;margin: auto;text-align: center;border-radius: 10px;">
-<p style="font-family: -apple-system, system-ui, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, sans-serif;font-size: 17px;line-height: 25px;margin: 0;">
-  Someone has sent you a secret. <br style="font-family: -apple-system, system-ui, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, sans-serif;">
-  Click to view it for one time. After that it will be deleted.
+<main style="background: white;padding: 30px 20px;max-width: 600px;margin: auto;text-align: center;border-radius: 10px;">
+<p style="font-size: 17px;line-height: 25px;margin: 0;">Hello, <br>
+  Someone has sent you a secret.
 </p>
-<a href="https://google.com" style="font-family: -apple-system, system-ui, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, sans-serif;text-decoration: none;"><button style="font-family: -apple-system, system-ui, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, sans-serif;all: unset;height: 48px;color: #ffffff;background-color: #0072f5;border-radius: 10px;padding: 0 40px;margin: 20px 0;will-change: transform;transition: all 0.25s;">
+<a href="https://google.com" style="text-decoration: none;"><button style="all: unset;height: 48px;color: #ffffff;background-color: #0072f5;border-radius: 10px;padding: 0 40px;margin: 20px 0;will-change: transform;transition: all 0.25s;">
     View Secret
   </button></a>
-<p style="font-family: -apple-system, system-ui, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, sans-serif;font-size: 17px;line-height: 25px;margin: 0;">
-  Thanks, <br style="font-family: -apple-system, system-ui, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, sans-serif;">
-  <a href="https://secret.hrvs.me" style="font-family: -apple-system, system-ui, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, sans-serif;text-decoration: none;color: #0072f5;">Secretly App</a>
+<p style="margin-bottom: 12px;font-size: 17px;line-height: 25px;margin: 0;">
+  Be aware, it will be deleted instantly after opening.
+</p>
+<p style="font-size: 17px;line-height: 25px;margin: 0;">
+  Thanks! <br>
+  <a href="https://secret.hrvs.me" style="text-decoration: none;color: #0072f5;">Secretly App</a>
 </p>
 </main>
-    `;
+`;
 };
 
 export default delivery;
