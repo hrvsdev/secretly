bd72c10ac4bafd26dbc17417f7a6af12cae6c53e
kingvyas <kumarvyasss@gmail.com>
2022-09-07 22:53:33 +0530

    changed send mail text to html

--- pages/api/send-mail.ts
+++ pages/api/send-mail.ts
@@ -21,7 +21,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       from: "Secretly <scrtly@hotmail.com>",
       to: email,
       subject: "It's a secret",
-      text: deliveryTemp(link),
+      html: deliveryTemp(link),
     });
     res.send(info);
   } catch (error) {
