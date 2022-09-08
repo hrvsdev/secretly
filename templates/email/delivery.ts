3a197f8eae117aa11f988ba5e06acc582a808717
kingvyas <kumarvyasss@gmail.com>
2022-09-08 12:33:00 +0530

    styled less margin problem

--- templates/email/delivery.ts
+++ templates/email/delivery.ts
@@ -9,7 +9,7 @@ const deliveryTemp = (link: string, message?: string) => {
     <title>Simple Call To Action</title>
 </head>
 
-<body style="padding: 25px 5px; background: #fbfcfe;">
+<body style="padding: 25px 10px; background: #fbfcfe;">
     <div class="main" style="max-width: 600px; width: 100%; margin: 0 auto;">
         <h1 style="margin: 0 0 20px; font-size: 32px; text-align: center;"><a href="https://secret.hrvs.me" style="text-decoration: none; color: #0072f5;">Secretly</a></h1>
         <div class="inner" style="box-sizing: border-box; width: 100%; background: #fff; border-radius: 10px; padding: 25px 10px; margin: 0 auto; border: 1px solid #e5eafa;">
