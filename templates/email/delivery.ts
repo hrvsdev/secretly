27cf41f4be0df0da42571daeb7533fba53d46629
kingvyas <kumarvyasss@gmail.com>
2022-09-08 09:28:02 +0530

    styled inline style to template

--- templates/email/delivery.ts
+++ templates/email/delivery.ts
@@ -1,74 +1,63 @@
 const deliveryTemp = (link: string, message?: string) => {
   return `
-  <style>
-      body {
-        background: #fafbfc;
-        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
-          "Helvetica Neue", Arial, sans-serif;
-      }
-
-      h1 {
-        text-align: center;
-        color: #0072f5;
-      }
-
-      main {
-        background: white;
-        padding: 30px 20px;
-        max-width: 600px;
-        margin: auto;
-        text-align: center;
-        border-radius: 10px;
-      }
-
-      p {
-        font-size: 17px;
-        line-height: 25px;
-        margin: 0;
-      }
-
-      a {
-        text-decoration: none;
-      }
-
-      button {
-        all: unset;
-        height: 48px;
-        color: #ffffff;
-        background-color: #0072f5;
-        border-radius: 10px;
-        padding: 0 40px;
+    <div
+      style="
+    background: #fafbfc;
+    padding: 25px 20px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  "
+    >
+      <h1 style="margin: 0 0 20px">
+        <a style="text-decoration: none; color: #0072f5" href="https://secret.hrvs.me">Secretly</a>
+      </h1>
+      <div
+        style="
+      max-width: 600px;
+      width: 100%;
+      background: #fff;
+      border-radius: 10px;
+      padding: 20px;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+    "
+      >
+        <p style="margin: 0; font-size: 16px; text-align: center; margin-bottom: 5px">
+          Hello Internet User,
+        </p>
+        <p style="margin: 0; font-size: 16px; text-align: center">
+          Someone sent you a secret. Click to see it.
+        </p>
+        <button
+          style="
         margin: 20px 0;
-        will-change: transform;
-        transition: all 0.25s;
-      }
-
-      button:hover {
-        filter: brightness(1.2);
-      }
-
-      button:active {
-        transform: scale(0.96);
-      }
-
-      p a {
-        color: #0072f5;
-      }
-    </style>
-
-    <h1>Secretly</h1>
-    <main>
-      <p>
-        Hello, <br />
-        Someone has sent you a secret.
-      </p>
-      <a href="${link}"><button>View Secret</button></a>
-      <p style="margin-bottom: 12px;">Be aware, it will be deleted instantly after opening.</p>
-      <p>
-        Thanks! <br />
-        <a href="https://secret.hrvs.me">Secretly App</a>
-      </p>
-    </main>
+        height: 48px;
+        width: 140px;
+        border-radius: 10px;
+        border: 0;
+        outline: 0;
+        cursor: pointer;
+        font-size: 16px;
+        background-color: #0072f5;
+      "
+        >
+          <a style="color: white; text-decoration: none" href="${link}">View Secret</a>
+        </button>
+        <p style="margin: 0; font-size: 16px; text-align: center; margin-bottom: 20px">
+          Be careful, the secret can be viewed only once.
+        </p>
+        <p style="margin: 0; font-size: 16px; text-align: center">
+          Thanks!<br /><a
+            style="color: #0072f5; text-decoration: none"
+            href="https://secret.hrvs.me"
+            >Secretly App</a
+          >
+        </p>
+      </div>
+    </div>
   `;
 };
 
