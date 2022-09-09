683a93aadfbd48740bd3d9420af4d6898e8c1902
kingvyas <kumarvyasss@gmail.com>
2022-09-09 14:39:21 +0530

    fix problem by error state on read receipt section

--- components/secret-input-section/options/read-receipt/ReadReceipt.tsx
+++ components/secret-input-section/options/read-receipt/ReadReceipt.tsx
@@ -11,7 +11,7 @@ export default function ReadReceipt(): JSX.Element {
 
   // Input change action
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    email.set(e.target.value);
+    email.val.set(e.target.value);
   };
 
   return (
@@ -21,7 +21,7 @@ export default function ReadReceipt(): JSX.Element {
         <AtIcon size={22} />
         <Input
           type="email"
-          value={email.value}
+          value={email.set.value}
           onChange={onChange}
           placeholder="username@email.com"
         />
