824c2451dd50c55a63e083eb0ce6cc1e9fc8c908
kingvyas <kumarvyasss@gmail.com>
2022-09-17 20:44:36 +0530

    setting state directly

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,5 +1,4 @@
 import styled from "@emotion/styled";
-import { useEffect } from "react";
 
 import Hero from "../hero";
 import Secret from "../secret-input-section";
@@ -10,15 +9,12 @@ export default function Home(): JSX.Element {
   // Hero props state
   const hero = useHero();
 
-  // Setting hero props on startup
-  useEffect(() => {
-    hero.set({
-      heading: "Here is a secret",
-      para: "Click the button below to view the secret. Copy it as it will be deleted instantly.",
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  
+  // Setting hero props
+  hero.set({
+    heading: "Share a secret",
+    para: "The secret is end-to-end encrypted and can only be viewed once via a link.",
+  });
+
   return (
     <Main>
       <Hero />
--- components/secret-view-section/SecretView.tsx
+++ components/secret-view-section/SecretView.tsx
@@ -14,7 +14,6 @@ import { deleteSecret, getSecret } from "../../firebase/db";
 import { getHash, decrypt } from "../../utils/utils";
 
 import type { SecretDataTypes } from "../../firebase/types";
-import { useEffect } from "react";
 
 export default function Link(): JSX.Element {
   // Router and URL link
@@ -103,14 +102,11 @@ export default function Link(): JSX.Element {
     isError: isPasswordIncorrect,
   };
 
-  // Setting hero props on startup
-  useEffect(() => {
-    hero.set({
-      heading: "Share a secret",
-      para: "The secret is end-to-end encrypted and can only be viewed once via a link.",
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // Setting hero props
+  hero.set({
+    heading: "Here is a secret",
+    para: "Click the button below to view the secret. Copy it as it will be deleted instantly.",
+  });
 
   return (
     <Main>
